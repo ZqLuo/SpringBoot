3e0f7654d4cc19125b5daf3c4169353fb6f6939b
540464544 <540464544@qq.com>
2017-04-08 00:11:08 +0800

    SpringBoot Init

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	@RequestMapping("/")
+	String index(){
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		//定制Banner,在Resource文件夹下新建banner.txt，http://patorjk.com/software/taag 生成内容
+
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		//关闭banner
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+	}
+}
