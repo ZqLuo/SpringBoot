34f0cc4b8e7ee26598ac60e40d12d46a27a16f21
540464544 <540464544@qq.com>
2017-04-09 21:13:20 +0800

    添加加密开启关闭配置

--- src/main/java/com/example/config/HTTPAutoToHTTPSConfiguration.java
+++ src/main/java/com/example/config/HTTPAutoToHTTPSConfiguration.java
@@ -5,6 +5,7 @@ import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +16,13 @@ import org.springframework.context.annotation.Configuration;
  */
 
 @Configuration
+@ConditionalOnProperty(value = "server.ssl.enabled",matchIfMissing = false) // 判断是否开启
 public class HTTPAutoToHTTPSConfiguration {
 
     @Value("${server.port}")
     private String serverPort;
+    @Value("${httpPort}")
+    private String httpPort;
 
     @Bean
     public EmbeddedServletContainerFactory servletContainer() {
@@ -44,7 +48,7 @@ public class HTTPAutoToHTTPSConfiguration {
 
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(8080);
+        connector.setPort(Integer.parseInt(httpPort));
         connector.setSecure(false);
         connector.setRedirectPort(Integer.parseInt(serverPort));
         return connector;
